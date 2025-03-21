d8e9823a244f684f7cf58ce8e51eacfe12e60cb9
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2025-03-21 17:24:22 +0530

    Update LambdaTestSetup.cs

--- TestProject1/Helper/LambdaTestSetup.cs
+++ TestProject1/Helper/LambdaTestSetup.cs
@@ -25,7 +25,7 @@ public class LambdaTestSetup
 
         var capabilities = new Dictionary<string, object?>
         {
-            { "browserName", "pw-chromium" },
+            { "browserName", "PW-Firefox" },
             { "browserVersion", "latest" },
             { "LT:Options", new Dictionary<string, string?>
                 {
