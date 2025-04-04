b9bcf90b28e5cbeabef6c5e56c2a5b9a4211cd8f
rachit-lambdatest <rachittiwari@lambdatest.com>
2025-04-04 17:21:37 +0530

    Update LambdaTestSetup.cs

--- TestProject1/Helper/LambdaTestSetup.cs
+++ TestProject1/Helper/LambdaTestSetup.cs
@@ -25,7 +25,7 @@ public class LambdaTestSetup
 
         var capabilities = new Dictionary<string, object?>
         {
-            { "browserName", "pw-firefox" },
+            { "browserName", "chrome" },
             { "browserVersion", "latest" },
             { "LT:Options", new Dictionary<string, string?>
                 {
